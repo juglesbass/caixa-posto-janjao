4fbbdd5ce5931688e8405f2777bfe829d4aa962a
Agildo <agildo@Agildos-Mac-Pro.bbrouter>
2026-08-25 13:29:40 -0400

    fix: adicionar import intl no summary_screen

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:printing/printing.dart';
 import 'package:share_plus/share_plus.dart';
 
